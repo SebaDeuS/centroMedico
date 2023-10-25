9a6ec246e79872f69605813f9625df20e0029ca3
Sebastian <sebam0974@gmail.com>
2023-10-25 00:45:58 -0300

    formulario terminado

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,28 @@
+import re
 from django import forms
+from itertools import cycle
+from django.core.exceptions import ValidationError
+
+def validar_rut(rut):
+  rut = rut.upper();
+  rut = rut.replace("-","")
+  rut = rut.replace(".","")
+  aux = rut[:-1]
+  dv = rut[-1:]
+  
+  revertido = map(int, reversed(str(aux)))
+
+  factors = cycle(range(2,8))
+
+  s = sum(d * f for d, f in zip(revertido,factors))
+
+  res = (-s)%11
+  if str(res) == dv:
+    return True
+  elif dv=="K" and res==10:
+    return True
+  else:
+    raise ValidationError("Rut invalido (ejemplo: 12345678-9)")
 
 class UsuarioForm(forms.Form):
     rut = forms.CharField(max_length=12)
@@ -6,9 +30,17 @@ class UsuarioForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     contraseña = forms.CharField(widget=forms.PasswordInput())
 
+'''
+def validar_rut_chileno(value):
+    rut_pattern = r'^\d{7,8}-[\dkK]$'
+
+    if not re.match(rut_pattern, value):
+        raise forms.ValidationError("Ingresa un RUT chileno válido (ejemplo: 12345678-9).")
+
+'''
 
 class RegistroForm(forms.Form):
-    rut = forms.CharField(label="RUT", max_length=10, min_length=10) 
+    rut = forms.CharField(label="RUT", max_length=10, min_length=10, validators=[validar_rut]) 
     nombre = forms.CharField(label="Nombre")
     email = forms.EmailField(label="Correo electrónico")
     password = forms.CharField(label="Contraseña", widget=forms.PasswordInput())
@@ -26,3 +58,5 @@ class RegistroForm(forms.Form):
 class LoginForm(forms.Form):
     email = forms.EmailField(label="Correo electrónico")
     password = forms.CharField(label="Contraseña", widget=forms.PasswordInput())
+
+    
